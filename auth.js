bed72b25e107da395536d59d16566955d86ee4b9
Taylor Krusen <taykrusen@gmail.com>
2017-12-19 14:28:55 -0800

    access token functionality working correctly

--- auth.js
+++ auth.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 // route redirecting to our authorization page
 app.get('/auth', (req, res) => {
     console.log(authorizationUri);
-    // res.redirect(authorizationUri);
+    res.redirect(authorizationUri);
 });
 // helper function to assemble authorization url
 function authorizeURL(params) {
@@ -31,4 +31,29 @@ const authorizationUri = authorizeURL({
     response_type: 'code',
     client_id: config.APP_CLIENT_ID,
     scope: 'CREATE_SHEETS WRITE_SHEETS'
+});
+// callback service parses the authorization code and requests access token
+app.get('/callback', (req, res) => {
+    const authCode = req.query.code;
+    const generated_hash = require('crypto')
+        .createHash('sha256')
+        .update(config.APP_SECRET + "|" + authCode)
+        .digest('hex');
+    const options = {
+        queryParameters: {
+            client_id: config.APP_CLIENT_ID,
+            code: authCode,
+            hash: generated_hash
+        }
+    };
+    smartsheet.tokens.getAccessToken(options, (error, result) => {
+        if (error) {
+            console.error('Access Token Error:', error.message);
+            return res.json('Authentication failed');
+        }
+        console.log('The resulting token: ', result);
+        return res
+            .status(200)
+            .json(result);
+    });
 });
